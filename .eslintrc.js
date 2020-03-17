b2265c2662bc0963f7d112762f8af4bbd42cd793
mornya <mornya78@gmail.com>
2020-03-18 01:38:58 +0900

    composition-api 적용 / lint 설정 / 디펜던시 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,42 +2,62 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    es6: true,
     node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
+    commonjs: true,
+    jest: true, // Set globally because it is not applied to overrides
   },
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
   plugins: [
     'standard',
     'vue',
   ],
   // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off',
-    'prefer-const': 'error',
-    'semi': 'off',
-    'yoda': 'off',
-    'no-return-assign': 'off',
-    'no-multi-spaces': 'off',
-    'spaced-comment': 'off',
-    'padded-blocks': 'off',
+    // for js
     'comma-dangle': 'off',
+    'prefer-const': 'error',
+    'no-console': 'off', // 라이브 배포시 빌드오류를 막기 위해 off 한다.
+    'no-debugger': 'off', // 라이브 배포시 빌드오류를 막기 위해 off 한다.
+    'no-inner-declarations': 'off',
+    'no-multi-spaces': 'off',
+    'no-return-assign': 'off',
     'no-unused-vars': 'off',
-    'no-console': 'off',
-    'vue/html-closing-bracket-spacing': 'off',
-    'vue/no-v-html': 'off',
-    'vue/require-prop-types': 'off',
-    'vue/require-default-prop': 'off',
+    'padded-blocks': 'off',
+    'semi': 'off',
+    'spaced-comment': 'off',
+    'yoda': 'off',
+    // for vue
     'vue/attribute-hyphenation': 'off',
-    'vue/order-in-components': 'error',
-    'vue/singleline-html-element-content': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
+    'vue/html-closing-bracket-spacing': 'off',
+    'vue/html-indent': ['error', 2],
     'vue/html-self-closing': 'off',
     'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/no-parsing-error': ['error', {
+      'control-character-in-input-stream': false,
+    }],
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/singleline-html-element-content': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    // for nuxt
+    'nuxt/no-cjs-in-config': 'off',
   },
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/src/plugins/vue-composition-api',
   ],
   /*
   ** Nuxt.js dev-modules
